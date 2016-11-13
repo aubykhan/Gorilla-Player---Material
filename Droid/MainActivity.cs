23867a0affc92daf718e7fbc2fbb01c3a744de3f
Asadullah.Yousuf <asadullah.yousuf@tpsonline.com>
2016-11-13 21:50:30 +0500

    Add project files.

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using UXDivers.Artina.Player.Droid;
+
+namespace UXDivers.Gorilla.Droid
+{
+	[Activity (Label = "Gorilla Player", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+		protected override void OnCreate (Bundle bundle)
+		{
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+
+			LoadApplication (Player.CreateApplication(ApplicationContext, new Config("Gorilla on Asadullah")));
+		}
+
+		public override bool OnKeyUp (Keycode keyCode, KeyEvent e)
+		{
+			if ((keyCode == Keycode.Menu || keyCode == Keycode.F1) && Coordinator.Instance != null) {
+				Coordinator.Instance.RequestStatusUpdate ();
+				return true; 
+			}
+
+			return base.OnKeyUp (keyCode, e); 
+		} 
+	}
+}
+
---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+
+using Foundation;
+using UIKit;
+
+[assembly: ExportRenderer(typeof(Page), typeof(UXDivers.Gorilla.ShakeDetectionRenderer))]
+namespace UXDivers.Gorilla.iOS
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init ();
+
+			// Code for starting up the Xamarin Test Cloud Agent
+			#if ENABLE_TEST_CLOUD
+			Xamarin.Calabash.Start();
+			#endif
+
+			LoadApplication (Player.CreateApplication(new Config("Gorilla on Asadullah")));
+
+			return base.FinishedLaunching (app, options);
+		}
+
+		public override void DidEnterBackground (UIApplication uiApplication)
+		{
+			base.DidEnterBackground (uiApplication);
+		}
+	}
+}
+
